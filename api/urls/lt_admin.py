a28df0c15dc254de8ce6ae321ac075367d859884
Yash Raj <ryash072007@gmail.com>
2024-10-28 20:21:12 +0400

    added admin login url

--- api/urls/lt_admin.py
+++ api/urls/lt_admin.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import LandingEndPoint
+from api.views import AdminSignInEndPoint, LandingEndPoint
 
 urlpatterns = [
-    path('', LandingEndPoint.as_view())
+    path('', LandingEndPoint.as_view()),
+    path('admin/login/', AdminSignInEndPoint.as_view())
 ]
