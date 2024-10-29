5f5ee00f4ac2e3c8859a01b13d278dc1db90e0dd
Yash Raj <ryash072007@gmail.com>
2024-10-29 17:14:58 +0400

    model serializer

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class StudentData(models.Model):
     studentName = models.CharField("Student Name",  max_length=30)
 
-    studentID = models.IntegerField("Student Unique Identifier", max_length=10)
+    studentID = models.IntegerField("Student Unique Identifier")
     studentPassword = models.CharField("Student account password", max_length=30)
 
     def __str__(self):
--- api/serializer.py
+++ api/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
+from api.models import StudentData
 
+class StudentLoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentData
+        fields = ["studentID", "studentPassword"]
