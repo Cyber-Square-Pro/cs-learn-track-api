f0a8242d276002bb34e30695e839d63d3d5d246b
Yash Raj <ryash072007@gmail.com>
2024-10-29 17:32:44 +0400

    fixed logical error where serializer was nto validated first

--- api/views/lt_admin.py
+++ api/views/lt_admin.py
@@ -13,14 +13,15 @@ class LandingEndPoint(APIView):
 
 class AdminSignInEndPoint(APIView):
     def post(self, request):
-        if not AdminData.objects.filter(username = request.data["username"]).exists():
-            return Response({"message": "Admin  does not exist", "status":  status.HTTP_400_BAD_REQUEST})
-        
         serializer = AdminDataSerializer(data = request.data)
         if not serializer.is_valid():
             return Response({"message": "Invalid data", "status":  status.HTTP_400_BAD_REQUEST})
+        admin_data = serializer.validated_data
+
+        if not AdminData.objects.filter(username = admin_data["username"]).exists():
+            return Response({"message": "Admin  does not exist", "status":  status.HTTP_400_BAD_REQUEST})
         
-        if AdminData.objects.get(username = request.data["username"]).password == request.data["password"]:
+        if AdminData.objects.get(username = admin_data["username"]).password == admin_data["password"]:
             return Response({"message": "Admin logged in successfully", "status":  status.HTTP_200_OK})
         else:
             return Response({"message": "Invalid password", "status":  status.HTTP_400_BAD_REQUEST})
