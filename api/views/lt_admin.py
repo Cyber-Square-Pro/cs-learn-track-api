62df8ee0b76ebddc512cfd7ed80661b39c6af8d9
Yash Raj <ryash072007@gmail.com>
2024-10-29 17:19:12 +0400

    working on StudentLoginEndPOint

--- api/views/lt_admin.py
+++ api/views/lt_admin.py
@@ -1,7 +1,15 @@
 from rest_framework.views import APIView
 from django.http import HttpResponse
-
+from rest_framework.response import Response
+from api.models import StudentData
+from api.serializer import StudentLoginSerializer
+from rest_framework import status
 
 class LandingEndPoint(APIView):
     def get(self,request):
         return HttpResponse('<h1 align="center" style="margin-top:200px">Welcome Onboard!..</h1>')
+
+
+class StudentLoginEndPoint(APIView):
+    def post(self, request):
+        serializer = StudentLoginSerializer(data=request.data)
