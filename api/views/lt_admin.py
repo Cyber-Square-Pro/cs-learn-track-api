07952aa00c95f763670bd5ec0d769f5e1ac4c0a6
Yash Raj <ryash072007@gmail.com>
2024-10-29 19:45:53 +0400

    tryna fix error

--- api/views/lt_admin.py
+++ api/views/lt_admin.py
@@ -12,4 +12,15 @@ class LandingEndPoint(APIView):
 
 class StudentLoginEndPoint(APIView):
     def post(self, request):
-        serializer = StudentLoginSerializer(data=request.data)
+        serializer = StudentLoginSerializer(data = request.data)
+        if not serializer.is_valid():
+            return Response({"message": "Invalid data", "status":  status.HTTP_400_BAD_REQUEST})
+        student_data = serializer.validated_data
+
+        if not StudentData.objects.filter(studentID = student_data["studentID"]).exists():
+            return Response({"message": "Student  does not exist", "status":  status.HTTP_400_BAD_REQUEST})
+        
+        if StudentData.objects.get(studentID = student_data["studentID"]).studentPassword == student_data["studentPassword"]:
+            return Response({"message": "Student logged in successfully", "status":  status.HTTP_200_OK})
+        else:
+            return Response({"message": "Invalid password", "status":  status.HTTP_400_BAD_REQUEST})
