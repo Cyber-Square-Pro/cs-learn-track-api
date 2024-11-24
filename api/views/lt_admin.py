d60d30a9b4c3bfec3d73b969eaddc99844bce263
Yash Raj <ryash072007@gmail.com>
2024-11-24 08:45:09 +0400

    Squashed commit of the following:
    commit 733d460e4791f31c3809977891128aaf1cb9eb5a
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Sun Nov 24 08:29:29 2024 +0400
    
        switch
    
    commit ad2188b34b94b7e490e82b06930ab7df75d2b8ee
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Nov 5 19:30:45 2024 +0400
    
        added view docstring
    
    commit b72d9af9b4adb33a25824bb79db98e6d589b38c4
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Fri Nov 1 19:59:24 2024 +0400
    
        Probably completed
    
    commit d834fb24458289ec4cd380abdc2812825f707bb0
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 20:14:27 2024 +0400
    
        fixed migration and student issue
    
    commit 4babc06a41a7c99123eeef8d8c630914295f21bf
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 20:00:20 2024 +0400
    
        preflush
    
    commit a501a24fd36bbe8c9c0181028fddb9f6324f05d4
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 19:57:25 2024 +0400
    
        MAHNN
    
    commit 07952aa00c95f763670bd5ec0d769f5e1ac4c0a6
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 19:45:53 2024 +0400
    
        tryna fix error
    
    commit 62df8ee0b76ebddc512cfd7ed80661b39c6af8d9
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 17:19:12 2024 +0400
    
        working on StudentLoginEndPOint
    
    commit 5f5ee00f4ac2e3c8859a01b13d278dc1db90e0dd
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 17:14:58 2024 +0400
    
        model serializer
    
    commit 658660620f9b1774e682e00f4dc396951117a8e0
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Oct 29 16:54:31 2024 +0400
    
        basic student model

--- api/views/lt_admin.py
+++ api/views/lt_admin.py
@@ -1,7 +1,43 @@
 from rest_framework.views import APIView
 from django.http import HttpResponse
-
+from rest_framework.response import Response
+from api.models import StudentData
+from api.serializer import StudentLoginSerializer
+from rest_framework import status
 
 class LandingEndPoint(APIView):
     def get(self,request):
         return HttpResponse('<h1 align="center" style="margin-top:200px">Welcome Onboard!..</h1>')
+
+
+class StudentLoginEndPoint(APIView):
+    """
+    API endpoint for student login.
+
+    This endpoint handles POST requests for student login. It validates the provided
+    student credentials and checks if the student exists in the database. 
+
+    Methods:
+        post(request): 
+            Handles the login process for students. It expects a JSON payload with 
+            'studentID' and 'studentPassword'. If the credentials are valid, it returns 
+            a success message; otherwise, it returns an appropriate error message.
+
+    Responses:
+        - 200 OK: If the student is successfully logged in.
+        - 400 Bad Request: If the provided data is invalid, the student does not exist, 
+          or the password is incorrect.
+    """
+    def post(self, request):
+        serializer = StudentLoginSerializer(data = request.data)
+        if not serializer.is_valid():
+            return Response({"message": "Invalid data", "status":  status.HTTP_400_BAD_REQUEST})
+        student_data = serializer.validated_data
+
+        if not StudentData.objects.filter(studentID = student_data["studentID"]).exists():
+            return Response({"message": "Student  does not exist", "status":  status.HTTP_400_BAD_REQUEST})
+        
+        if StudentData.objects.get(studentID = student_data["studentID"]).studentPassword == student_data["studentPassword"]:
+            return Response({"message": "Student logged in successfully", "status":  status.HTTP_200_OK})
+        else:
+            return Response({"message": "Invalid password", "status":  status.HTTP_400_BAD_REQUEST})
