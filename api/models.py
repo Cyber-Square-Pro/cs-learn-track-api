1e3ebee4317859d72d5677518b838330da6e17a8
Yash Raj <ryash072007@gmail.com>
2024-12-12 20:16:24 +0400

    uniqued the id

--- api/models.py
+++ api/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 class StudentData(models.Model):
     studentName = models.CharField("Student Name",  max_length=30)
-
-    studentID = models.IntegerField("Student Unique Identifier")
+    studentID = models.IntegerField("Student Unique Identifier", unique=True)
     studentPassword = models.CharField("Student account password", max_length=30)
 
     def __str__(self):
