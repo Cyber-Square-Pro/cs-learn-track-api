eb010f1408c8f0e84fd9cca9691dc61f53e7b4c1
Yash Raj <ryash072007@gmail.com>
2024-12-12 20:50:18 +0400

    added stuff to StudentData

--- api/models.py
+++ api/models.py
@@ -5,17 +5,21 @@ class StudentData(models.Model):
     studentName = models.CharField("Student Name",  max_length=30)
     studentID = models.IntegerField("Student Unique Identifier", unique=True)
     studentPassword = models.CharField("Student account password", max_length=30)
+    studentBatch = models.ForeignKey("Batch", on_delete=models.CASCADE, related_name='students')
+    studentPFP = models.ImageField("Student Profile Picture", upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
         return self.studentName
 
-class Batch(models.Model):
-    name = models.CharField(max_length=100)
-    schedule = models.TextField()  # Ask about it, unsure how to implement
-    description = models.TextField(blank=True, null=True)
+class Batch(models.Model): # ADD No of seats and if batch is active # Remov name from creation endpoint # POst after changed # CLass teacher as foreign key
     grade = models.CharField(max_length=10)
     section = models.CharField(max_length=10)
-    students = models.ManyToManyField(StudentData, related_name='batches')
+    seat_number = models.IntegerField("Number of seats available")
+
+    active_batch = models.BooleanField("Is the batch active", default=True)
+    schedule = models.TextField()  # Ask about it, unsure how to implement
+    description = models.TextField(blank=True, null=True)
+
 
     def __str__(self):
         return self.name
