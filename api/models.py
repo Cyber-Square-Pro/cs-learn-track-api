8a385d5d82aaa9e26ec633f50ef5ecee9f2720b6
Yash Raj <ryash072007@gmail.com>
2024-12-17 20:50:04 +0400

    Squashed commit of the following:
    commit 72cb628e6b6c21e3357a1aa9092816b4cee5d4c3
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Dec 17 20:44:16 2024 +0400
    
        made migrations
    
    commit 26471403dd6cd7c13f10a52d9d3c0a73b22b69fc
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Tue Dec 17 20:28:17 2024 +0400
    
        updated studentData model to include fields mentioned in whatsapp grp
    
    commit 2d72721fce45216823ee8b9c60579ca96e78472a
    Author: Yash Raj <ryash072007@gmail.com>
    Date:   Thu Dec 12 21:07:00 2024 +0400
    
        Squashed commit of the following:
    
        commit 1b755bdb4bec4ccd9033444eece89700f1313c22
        Author: Yash Raj <ryash072007@gmail.com>
        Date:   Thu Dec 12 21:06:33 2024 +0400
    
            updated

--- api/models.py
+++ api/models.py
@@ -2,24 +2,32 @@ from django.db import models
 
 # Create your models here.
 class StudentData(models.Model):
-    studentName = models.CharField("Student Name",  max_length=30)
-    studentID = models.IntegerField("Student Unique Identifier", unique=True)
+    studentName = models.CharField("Student Name", max_length=30)
+    admissionNo = models.IntegerField("Admission Number", unique=True)
+    rollNo = models.IntegerField("Roll Number")
+    studentClass = models.CharField("Class", max_length=10)
+    division = models.CharField("Division", max_length=10)
+    gender = models.CharField("Gender", max_length=10)
+    fatherName = models.CharField("Father's Name", max_length=30)
+    email = models.EmailField("Email", unique=True)
+    contactNo = models.CharField("Contact Number", max_length=15)
+    joinedDate = models.DateField("Joined Date")
+    accountStatus = models.BooleanField("Account Status", default=True)
     studentPassword = models.CharField("Student account password", max_length=30)
-    studentBatch = models.ForeignKey("Batch", on_delete=models.CASCADE, related_name='students')
-    studentPFP = models.ImageField("Student Profile Picture", upload_to='profile_pictures/', blank=True, null=True)
+    batch = models.ForeignKey("Batch", on_delete=models.CASCADE, related_name='students')
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    profilePic = models.ImageField("Profile Picture", upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
         return self.studentName
 
-class Batch(models.Model): # CLass teacher as foreign key
+class Batch(models.Model):
     grade = models.CharField(max_length=10)
     section = models.CharField(max_length=10)
-    seat_number = models.IntegerField("Number of seats available")
-
-    active_batch = models.BooleanField("Is the batch active", default=True)
-    schedule = models.TextField()  # Ask about it, unsure how to implement
+    seatNumber = models.IntegerField("Number of seats available")
+    activeBatch = models.BooleanField("Is the batch active", default=True)
+    schedule = models.TextField()
     description = models.TextField(blank=True, null=True)
-
 
     def __str__(self):
         return self.name
--- api/views/lt_admin.py
+++ api/views/lt_admin.py
@@ -6,9 +6,8 @@ from api.serializer import StudentLoginSerializer, BatchCreationSerializer, Stud
 from rest_framework import status
 
 class LandingEndPoint(APIView):
-    def get(self,request):
+    def get(self, request):
         return HttpResponse('<h1 align="center" style="margin-top:200px">Welcome Onboard!..</h1>')
-
 
 class StudentLoginEndPoint(APIView):
     """
@@ -20,7 +19,7 @@ class StudentLoginEndPoint(APIView):
     Methods:
         post(request): 
             Handles the login process for students. It expects a JSON payload with 
-            'studentID' and 'studentPassword'. If the credentials are valid, it returns 
+            'admissionNo' and 'studentPassword'. If the credentials are valid, it returns 
             a success message; otherwise, it returns an appropriate error message.
 
     Responses:
@@ -29,18 +28,18 @@ class StudentLoginEndPoint(APIView):
           or the password is incorrect.
     """
     def post(self, request):
-        serializer = StudentLoginSerializer(data = request.data)
+        serializer = StudentLoginSerializer(data=request.data)
         if not serializer.is_valid():
-            return Response({"message": "Invalid data", "status":  status.HTTP_400_BAD_REQUEST})
+            return Response({"message": "Invalid data", "status": status.HTTP_400_BAD_REQUEST})
         student_data = serializer.validated_data
 
-        if not StudentData.objects.filter(studentID = student_data["studentID"]).exists():
-            return Response({"message": "Student  does not exist", "status":  status.HTTP_400_BAD_REQUEST})
+        if not StudentData.objects.filter(admissionNo=student_data["admissionNo"]).exists():
+            return Response({"message": "Student does not exist", "status": status.HTTP_400_BAD_REQUEST})
         
-        if StudentData.objects.get(studentID = student_data["studentID"]).studentPassword == student_data["studentPassword"]:
-            return Response({"message": "Student logged in successfully", "status":  status.HTTP_200_OK})
+        if StudentData.objects.get(admissionNo=student_data["admissionNo"]).studentPassword == student_data["studentPassword"]:
+            return Response({"message": "Student logged in successfully", "status": status.HTTP_200_OK})
         else:
-            return Response({"message": "Invalid password", "status":  status.HTTP_400_BAD_REQUEST})
+            return Response({"message": "Invalid password", "status": status.HTTP_400_BAD_REQUEST})
 
 class BatchCreationEndPoint(APIView):
     def post(self, request):
