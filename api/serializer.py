26471403dd6cd7c13f10a52d9d3c0a73b22b69fc
Yash Raj <ryash072007@gmail.com>
2024-12-17 20:28:17 +0400

    updated studentData model to include fields mentioned in whatsapp grp

--- api/serializer.py
+++ api/serializer.py
@@ -4,14 +4,18 @@ from api.models import StudentData, Batch
 class StudentLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentData
-        fields = ["studentID", "studentPassword"]
+        fields = ["admissionNo", "studentPassword"]
 
 class BatchCreationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Batch
-        fields = ["description", "grade", "section", "seat_number"]
+        fields = ["description", "grade", "section", "seatNumber"]
 
 class StudentRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentData
-        fields = ["studentName", "studentID", "studentPassword", "studentBatch"]
+        fields = [
+            "studentName", "admissionNo", "rollNo", "studentClass", "division", 
+            "gender", "fatherName", "email", "contactNo", "joinedDate", 
+            "accountStatus", "studentPassword", "batch", "profilePic"
+        ]
