292a5c26a54044c260a8a9277a6d44039248db7a
Yash Raj <ryash072007@gmail.com>
2025-01-14 12:33:28 +0400

    made batchName unique

--- db/models/batch.py
+++ db/models/batch.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Batch(models.Model):
     id = models.AutoField(primary_key=True)
-    batchName = models.CharField(max_length=50)
+    batchName = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True, null=True)
     batchStatus = models.BooleanField("Batch Status", default=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
